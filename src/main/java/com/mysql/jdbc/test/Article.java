28941af984871aec29d9595671cbdd2c5355285a
sik2 <devpang20@gmail.com>
2023-04-20 00:41:26 +0900

    JDBC select 테스트

---
+++ src/main/java/com/mysql/jdbc/test/Article.java
@@ -0,0 +1,30 @@
+package com.mysql.jdbc.test;
+
+public class Article {
+    public int id;
+    public String title;
+    private String body;
+    private String regDate;
+    private String updateDate;
+
+    public Article(int id, String regDate, String updateDate, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Article(int id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
